7827af107da937048cdcedb3a312e034ad9313db
sabina <sabisop@gmail.com>
2020-09-22 16:04:03 -0500

    gitignore

--- mvp/webpack.config.js
+++ mvp/webpack.config.js
@@ -1,3 +1,4 @@
+//this points to index.jsx in client folder
 const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
